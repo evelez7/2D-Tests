2e62e66f1d3239cde31b2011d57fd9f079cc07cd
Erick Velez <erickvelez7@gmail.com>
2021-09-24 11:41:24 -0700

    Remove repeated grid point var, test interpolation function

--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -73,26 +73,6 @@ vector<array<double, DIM>> compute_points_to_consider(const array<double, DIM> &
   return points_to_consider;
 }
 
-double interpolate(const double& q_k, const array<double, DIM> x_k, const double& h_g, const int& choice)
-{
-  array<double, DIM> left_hand_corner = { floor(x_k[0] / h_g), floor(x_k[1] / h_g) };
-  auto points_to_consider = compute_points_to_consider(left_hand_corner, choice);
-  double sum = 0.;
-  for (auto current_point : points_to_consider)
-  {
-    array<double, DIM> x_bar = { current_point[0] * h_g, current_point[1] * h_g };
-
-    array<double, DIM> z = { x_bar[0] - x_k[0], x_bar[1] - x_k[1] };
-    // array<double, DIM> z = { x_k[0] - x_bar[0], x_k[1] - x_bar[1] };
-
-    double w_result = w(z, h_g, choice);
-    // cout << "w_result: " << w_result << endl;
-    sum += q_k * w_result;
-  }
-  // cout << "sum: " << sum << endl;
-  return sum;
-}
-
 void interpolate(BoxData<double>& grid, const double& q_k, const array<double, DIM> x_k, const double& hg, const double& hp, const int& choice)
 {
   array<double, DIM> left_hand_corner = { floor(x_k[0] / hg), floor(x_k[1] / hg) };
@@ -110,8 +90,32 @@ void interpolate(BoxData<double>& grid, const double& q_k, const array<double, D
     // array<double, DIM> scaled_grid_point { current_point[0] * hg, current_point[1] * hg };
     if (grid.box().contains(to_store))
     {
-      if ((scaled_grid_point[0] <= limit && scaled_grid_point[0] >= -limit) && (scaled_grid_point[1] <= limit && scaled_grid_point[1] >= -limit))
+      if ((x_bar[0] <= limit && x_bar[0] >= -limit) && (x_bar[1] <= limit && x_bar[1] >= -limit))
         grid(to_store) += q_k * w_result;
     }
   }
+}
+
+// function to test whether or not the contribution of each grid point adds up to the total interpolated value
+void interpolate_test(const double& q_k, const array<double, DIM> x_k, const double& hg, const double& hp, const int& choice)
+{
+  array<double, DIM> left_hand_corner = { floor(x_k[0] / hg), floor(x_k[1] / hg) };
+  auto points_to_consider = compute_points_to_consider(left_hand_corner, choice);
+  double interpolated_value = 0.;
+  for (auto current_point : points_to_consider)
+  {
+    double contribution = 0.;
+    array<double, DIM> x_bar = { current_point[0] * hg, current_point[1] * hg };
+
+    array<double, DIM> z = { x_bar[0] - x_k[0], x_bar[1] - x_k[1] };
+    // array<double, DIM> z = { x_k[0] - x_bar[0], x_k[1] - x_bar[1] };
+
+    double w_result = w(z, hg, choice);
+    Point to_store { static_cast<int>(current_point[0]), static_cast<int>(current_point[1]) };
+    contribution = q_k * w_result;
+    cout << "contribution: " << contribution << endl;
+    interpolated_value += contribution;
+  }
+  cout << "full: " << interpolated_value << endl;
+
 }
--- src/interpolate.h
+++ src/interpolate.h
@@ -8,13 +8,8 @@
 using namespace std;
 using namespace Proto;
 
-// array<std::array<double, DIM>, DIM> interpolate(const Proto::BoxData<double> [DIM][DIM], const Particle&, const double&);
-
-// array<double, DIM> interpolate(const array<double, DIM>&, const array<double, DIM>&, const array<double, DIM>, const double&, const int&);
-
-double interpolate(const double&, const array<double, DIM>, const double&, const int&);
-
 void interpolate(BoxData<double>&, const double&, const array<double, DIM>, const double&, const double&, const int&);
 
+void interpolate_test(const double&, const array<double, DIM>, const double&, const double&, const int&);
 
 #endif
