30c66e4c94d76010a896a693d7a57112ffd51b76
Colin Wahl <colinpwahl@gmail.com>
2021-10-01 15:09:25 -0700

    Rewrote compute_points_to_consider
    Found a bug that seemed to be causing some of the rigid body
    interpolation issues. The current implementation works for 2D but
    won't generalize to arbitrary dimensions.

--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -15,61 +15,19 @@ std::map<double (*)(double), std::tuple<int, int>> r_map = {
   {L4_4, std::make_tuple(-2, 3)}
 };
 
-const double limit = 0.7;
-
-bool point_is_present_in_vector(const array<double, DIM>& point_to_find,
-                                const vector<array<double, DIM>>& vec) {
-  if (find(vec.begin(), vec.end(), point_to_find) != vec.end()) {
-    return true;
-  }
-  return false;
-}
-
-void complete_grid(vector<array<double, DIM>>& points_to_consider, const array<double, DIM>& to_add) {
-  if (!point_is_present_in_vector(to_add, points_to_consider)) {
-    // if the point is present, then its points to consider must already be within
-    points_to_consider.push_back(to_add);
-    return;
-  }
-
-  array<double, DIM> new_1 = {to_add[0] + 1, to_add[1]};
-  if (!point_is_present_in_vector(new_1, points_to_consider)) {
-    points_to_consider.push_back(new_1);
-  }
-
-  array<double, DIM> new_2 = {to_add[0], to_add[1] + 1};
-  if (!point_is_present_in_vector(to_add, points_to_consider)) {
-    points_to_consider.push_back(new_2);
-  }
-
-  array<double, DIM> new_3 = {to_add[0] + 1, to_add[1]};
-  if (!point_is_present_in_vector(to_add, points_to_consider)) {
-    points_to_consider.push_back(new_3);
-  }
-}
-
 vector<array<double, DIM>> compute_points_to_consider(const array<double, DIM> &original_point, const int& choice) {
-  // cout << "before rmap" << endl;
   auto r_tuple = r_map[get_w(choice)];
-  // cout << "after rmap" << endl;
   auto r_low = get<0>(r_tuple);
   auto r_high = get<1>(r_tuple);
 
   vector<array<double, DIM>> points_to_consider;
-  points_to_consider.push_back(original_point);
 
-  for (int i = r_low; i <= r_high; ++i) {
-    array<double, DIM> new_1 = {original_point[0] + i, original_point[1]};
-    // points_to_consider.push_back(new_1);
-    complete_grid(points_to_consider, new_1);
-    array<double, DIM> new_2 = {original_point[0], original_point[1] + i};
-    // points_to_consider.push_back(new_2);
-    complete_grid(points_to_consider, new_2);
-    array<double, DIM> new_3 = {original_point[0] + i, original_point[1] + i};
-    // points_to_consider.push_back(new_);
-    complete_grid(points_to_consider, new_3);
+  for (int i = r_low; i <= r_high; i++) {
+      for (int j = r_low; j <= r_high; j++) {
+          array<double, DIM> new_1 = {original_point[0] + i, original_point[1]+j};
+          points_to_consider.push_back(new_1);
+      }
   }
-
   return points_to_consider;
 }
 
@@ -90,7 +48,6 @@ void interpolate(BoxData<double>& grid, const double& q_k, const array<double, D
     // array<double, DIM> scaled_grid_point { current_point[0] * hg, current_point[1] * hg };
     if (grid.box().contains(to_store))
     {
-      if ((x_bar[0] <= limit && x_bar[0] >= -limit) && (x_bar[1] <= limit && x_bar[1] >= -limit))
         grid(to_store) += q_k * w_result;
     }
   }
